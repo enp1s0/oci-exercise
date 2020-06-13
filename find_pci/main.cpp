3cc879f9cddaa8c4222e12ce6c4aa90e0ad0d3c9
mutsuki <mutsuki@momo86.net>
2020-06-13 17:34:05 +0900

    Add printing more information

--- find_pci/main.cpp
+++ find_pci/main.cpp
@@ -12,13 +12,25 @@ int main() {
 	p_access = pci_alloc();
 	pci_init(p_access);
 	pci_scan_bus(p_access);
-	std::printf("vender_id:device_id\n");
 	for (p_dev = p_access->devices; p_dev; p_dev = p_dev->next) {
 		pci_fill_info(p_dev, PCI_FILL_IDENT | PCI_FILL_BASES);
 		const unsigned device_id = p_dev->device_id;
 		const unsigned vender_id = p_dev->vendor_id;
 
-		std::printf("%9x:%9x\n", vender_id, device_id);
+		const unsigned bus = p_dev->bus;
+		const unsigned dev = p_dev->dev;
+		const unsigned func = p_dev->func;
+
+		char class_name[128];
+		pci_lookup_name(p_access, class_name, sizeof(class_name), PCI_LOOKUP_CLASS, p_dev->device_class);
+
+		char device_name[128];
+		pci_lookup_name(p_access, device_name, sizeof(device_name), PCI_LOOKUP_DEVICE, p_dev->device_id);
+
+		char vender_name[128];
+		pci_lookup_name(p_access, vender_name, sizeof(vender_name), PCI_LOOKUP_VENDOR, p_dev->vendor_id);
+
+		std::printf("%x:%x, path = %02x.%02x.%d, class_name = %40s, device_name = %20s, vender_name = %20s\n", vender_id, device_id, bus, dev, func, class_name, device_name, vender_name);
 	}
 
 	pci_cleanup(p_access);
