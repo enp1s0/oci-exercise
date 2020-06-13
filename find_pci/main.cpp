a4bc73b05400311bb43a6071a7e00778ba777ae9
mutsuki <mutsuki@momo86.net>
2020-06-13 17:14:14 +0900

    Add printing device_id and vender_id

--- find_pci/main.cpp
+++ find_pci/main.cpp
@@ -12,8 +12,13 @@ int main() {
 	p_access = pci_alloc();
 	pci_init(p_access);
 	pci_scan_bus(p_access);
+	std::printf("vender_id:device_id\n");
 	for (p_dev = p_access->devices; p_dev; p_dev = p_dev->next) {
 		pci_fill_info(p_dev, PCI_FILL_IDENT | PCI_FILL_BASES);
+		const unsigned device_id = p_dev->device_id;
+		const unsigned vender_id = p_dev->vendor_id;
+
+		std::printf("%9x:%9x\n", vender_id, device_id);
 	}
 
 	pci_cleanup(p_access);
